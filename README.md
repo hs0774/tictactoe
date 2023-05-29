e0aed4351fa58b4912422f874eff1598ff6af848
Hardeep Singh <hs0774@gmail.com>
2023-05-28 22:59:26 -0400

    winner checked,turns changed,clear and play again buttons work,project is finished

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 Tic tac toe project the goal is to use as little global variables as
 i could. 
 
-changes per commit:
+changes commited:
 buttons now change color when clicked
 a placeholder x or o pops on the divs 
+turn switching 
 a placehold x or o will not change if clicked again 
+win check 
+clear and play again buttons 
+toggle modal to announce winner 
+proper winner is announced 
+
+finished
--- script.js
+++ script.js
@@ -8,13 +8,19 @@ const six = document.getElementById('six');
 const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
 
-const gameBoard = [
+const clearbtn = document.querySelector('.clear');
+const playagainbtn = document.querySelector('.play-again');
+const xbtn = document.querySelector('.x');
+const modal = document.querySelector('.winner-modal');
+const roundWinner=document.querySelector('.round-winner');
+
+let gameBoard = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ];
   
-const tictactoeArr = [
+let tictactoeArr = [
   [zero, one, two],
   [three, four, five],
   [six, seven, eight],
@@ -24,8 +30,8 @@ const tictactoeArr = [
 const player = function(player){
     this.player=player;
 }
-const player1 = new player('x');
-const player2 = new player('o');
+const player1 = new player('X');
+const player2 = new player('O');
 let toggle=0;
 
 tictactoeArr.forEach((row,rowIndex) => {
@@ -36,21 +42,79 @@ tictactoeArr.forEach((row,rowIndex) => {
             if(toggle%2==0){
             element.textContent=player1.player;
             gameBoard[rowIndex][columnIndex] = player1.player;
+            checkWinner(player1);
             toggle++;
             }
             else{
             element.textContent=player2.player;
             gameBoard[rowIndex][columnIndex] = player2.player;
+            checkWinner(player2);
             toggle++;
             }
-            checkwinner();
         }
        });
    });
 });
-const checkwinner = function(){
-  console.log('checking');
-  if(toggle==9){
-    alert('tie');
+
+function checkWinner(player) {
+    const win =[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for (const condition of win) {
+      const [a, b, c] = condition;
+      if  (gameBoard[Math.floor(a / 3)][a % 3] === player.player &&
+      gameBoard[Math.floor(b / 3)][b % 3] === player.player &&
+      gameBoard[Math.floor(c / 3)][c % 3] === player.player) {
+        if(player.player==='X'){
+             roundWinner.textContent=`Player 1 wins! Start clicking to play again`;
+        }
+        else if(player.player==='O') {
+            roundWinner.textContent=`Player 2 wins! Start clicking to play again`;
+        }
+
+        modal.classList.toggle('hidden');
+        clearboard();
+        return true;
+      }
+    }
+    if (toggle === 8) {
+        roundWinner.textContent = `It's a tie! Start clicking to play again`;
+        modal.classList.toggle('hidden');
+        clearboard();
+        return true;
+      }
+
+    return false;
   }
-}
+
+  function clearboard(){
+    gameBoard = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ];
+      toggle = 0;
+      tictactoeArr.forEach((row,rowIndex) => {
+        row.forEach((element,columnIndex) => {
+          element.textContent = '';
+        });
+      });
+  }
+clearbtn.addEventListener('click',function(){
+    clearboard();
+ });
+
+playagainbtn.addEventListener('click',function(){
+    clearboard();
+}); 
+
+xbtn.addEventListener('click',function(){
+    modal.classList.toggle('hidden');
+});
